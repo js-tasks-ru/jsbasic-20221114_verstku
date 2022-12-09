e4b6c0a4dbbdfd05f0cb1c264a4a76c5798aadac
Yurii Ruban <verstku@gmail.com>
2022-12-09 14:20:56 +0200

    4-module: 3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,20 @@
 function highlight(table) {
-  // ваш код...
+  for (let tableRow of table.tBodies[0].rows) {
+    if (tableRow.cells[3].dataset.available && tableRow.cells[3].dataset.available == 'true') {
+      tableRow.classList.add('available')
+    } else if (tableRow.cells[3].dataset.available && tableRow.cells[3].dataset.available == 'false') {
+      tableRow.classList.add('unavailable');
+    }
+
+    if (!tableRow.cells[3].dataset.available) tableRow.setAttribute('hidden', '');
+
+    if (tableRow.cells[2].textContent == 'm') {
+      tableRow.classList.add('male')
+    } else if (tableRow.cells[2].textContent == 'f') {
+      tableRow.classList.add('female')
+    }
+
+    if (tableRow.cells[1].textContent < 18) tableRow.style.textDecoration = 'line-through';
+
+  }
 }
