d96bfdfa8526d7eb59854b09755cd74fb86ed366
Yurii Ruban <verstku@gmail.com>
2022-12-11 21:57:57 +0200

    5-module: 2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector('.toggle-text-button');
+
+  btn.addEventListener('click', function () {
+    (text.hasAttribute('hidden')) ? text.removeAttribute('hidden') : text.setAttribute('hidden', '');
+  }, false);
 }
